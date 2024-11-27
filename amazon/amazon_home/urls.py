745ae6fc5a0f4adbe519222f65324140146fac09
Bhaskara Veda Vishnu Datta <vedabhaskara@gmail.com>
2024-11-27 12:35:02 +0530

    Delete amazon directory

--- amazon/amazon_home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path("home", views.method),
-    path("home2", views.method2),
-    path("home3", views.method3),
-    path("home4", views.method4),
-    ]
